8a27900f1ceae613e7bda3db6854f82e4ae3cd50
smithmayowa20 <smithmayowa20@yahoo.com>
2017-08-05 16:39:43 +0100

    changed allowed host

--- skills/settings.py
+++ skills/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3yee69=rpnjja-&jpm5*p()jsmg_l8rl5tlq=k9j8ouz!pxl!^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
