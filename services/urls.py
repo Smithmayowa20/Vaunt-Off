5efa62e33bb7d2da423437071e1a0c5aac814fa6
smithmayowa20 <smithmayowa20@yahoo.com>
2017-08-05 09:29:51 +0100

    My First Vaunt-Off App, first commit

---
+++ services/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from . import views
+from django.views.generic import (TemplateView, 
+    RedirectView,
+)
+from django.conf import settings
+urlpatterns = [url(r'^profile/edit/$',views.edit_profile_page,name='edit_profile_page'),
+url(r'^post/new-comment/(?P<slug>[-\w]+)/$', views.new_comment, name='new_comment1'),
+url(r'^post/new-comment/(?P<slug>[-\w]+)/(?P<position>[-\w]+)/(?P<parent_slug>[-\w]+)/$', views.new_comment1, name='new_comment'),
+url(r'^profile/$',views.profile_page,name='profile_page'),
+url(r'^profile/(?P<user>[-\w]+)$',views.profile_page,name='profile_page2'),
+url(r'^$',views.landing_page,name='landing_page'),
+url(r'^category/(?P<category>[-\w]+)$',views.genre_category,name='genre_category'),
+url(r'^post/(?P<slug>[-\w]+)$',views.post_detail,name='post_detail'),
+url(r'^post/(?P<user1>[-\w]+)$',views.create_follower_following,name='follower_following'),]
