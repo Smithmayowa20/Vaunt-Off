d53e374008b0ad4067f15b5d1dc142ba78182704
smithmayowa20 <smithmayowa20@yahoo.com>
2017-08-07 18:50:18 +0100

    final fucking chance heroku2

--- skills/wsgi.py
+++ skills/wsgi.py
@@ -8,7 +8,8 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "proyecto_www.settings.staging")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "skills.settings")
+
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-application = Cling(get_wsgi_application())
+
+application = get_wsgi_application()
